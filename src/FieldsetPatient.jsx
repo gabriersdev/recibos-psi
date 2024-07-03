43352b4856388d28b0daf48838dd2964ad6dbea7
Gabriel <105074547+gabrieszin@users.noreply.github.com>
2024-07-03 09:15:07 -0300

    feat: Add month selection dropdown to FieldsetPatient component
    The code changes add a dropdown menu to select the month in the FieldsetPatient component. This allows the user to choose the month for the sessions.

--- src/FieldsetPatient.jsx
+++ src/FieldsetPatient.jsx
@@ -12,9 +12,15 @@ import { useEffect } from "react";
 import Autocomplete from "@mui/material/Autocomplete";
 import Intermediate from './Intermediate';
 
+// Imports para Select
+import InputLabel from '@mui/material/InputLabel';
+import MenuItem from '@mui/material/MenuItem';
+import FormControl from '@mui/material/FormControl';
+import Select from '@mui/material/Select';
+
 // Get days of the month
 const days = Array.from({ length: moment().daysInMonth() }, (_, i) => i + 1);
-const optionsAutocomplete = days.map((day) => { return { key: day, value: `${('0' + day).slice(-2)}/${('0' + (moment().month() + 1)).slice(-2)}` } });
+const optionsAutocomplete = days.map((day) => { return { key: day, value: `${('0' + day).slice(-2)}` } });
 
 function FieldsetPatient(attr) {
   const [name, setName] = useState('Roberto');
@@ -22,6 +28,12 @@ function FieldsetPatient(attr) {
   const [valorSessao, setValorSessao] = useState('1.000,00');
   
   const [options, setOptions] = useState([]);
+
+  const [age, setAge] = useState(moment().get('month'));
+
+  const handleChange = (event) => {
+    setAge(event.target.value);
+  };
 
   useEffect(() => {
     setOptions(optionsAutocomplete);
@@ -68,6 +80,34 @@ function FieldsetPatient(attr) {
             <TextField {...params} key={index} label="Dias das sessões*" variant="outlined" />
           )}
         />
+
+        <FormControl sx={{ m: 1, minWidth: 120 }}>
+        <InputLabel id="demo-simple-select-helper-label">Mês*</InputLabel>
+        <Select
+          labelId="demo-simple-select-helper-label"
+          id="demo-simple-select-helper"
+          value={age}
+          label="Age"
+          onChange={handleChange}
+        >
+          <MenuItem value="">
+            <em>Selecione</em>
+          </MenuItem>
+          <MenuItem value="0">Janeiro</MenuItem>
+          <MenuItem value="1">Fevereiro</MenuItem>
+          <MenuItem value="2">Março</MenuItem>
+          <MenuItem value="3">Abril</MenuItem>
+          <MenuItem value="4">Maio</MenuItem>
+          <MenuItem value="5">Junho</MenuItem>
+          <MenuItem value="6">Julho</MenuItem>
+          <MenuItem value="7">Agosto</MenuItem>
+          <MenuItem value="8">Setembro</MenuItem>
+          <MenuItem value="9">Outubro</MenuItem>
+          <MenuItem value="10">Novembro</MenuItem>
+          <MenuItem value="11">Dezembro</MenuItem>
+        </Select>
+      </FormControl>  
+      <TextField id="pat-ano" label="Ano" variant="outlined" value={2024} required={attr.required}/>
       </Box>
     </fieldset>
   );
