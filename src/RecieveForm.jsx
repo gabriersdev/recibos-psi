a9ff8dbea3f6251bdda81785bbb45ed0de83a307
Gabriel <105074547+gabrieszin@users.noreply.github.com>
2024-07-24 08:57:06 -0300

    fix: corrigindo erro de importação da fonte para PDF make

--- src/RecieveForm.jsx
+++ src/RecieveForm.jsx
@@ -17,8 +17,16 @@ import Paciente from './Paciente';
 import Recibo from './Recibo';
 
 import pdfMake from "pdfmake/build/pdfmake";
-import font from "pdfmake/build/vfs_fonts";
-pdfMake.vfs = font;
+
+pdfMake.fonts = {
+  // download default Roboto font from cdnjs.com
+  Roboto: {
+    normal: 'https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.66/fonts/Roboto/Roboto-Regular.ttf',
+    bold: 'https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.66/fonts/Roboto/Roboto-Medium.ttf',
+    italics: 'https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.66/fonts/Roboto/Roboto-Italic.ttf',
+    bolditalics: 'https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.66/fonts/Roboto/Roboto-MediumItalic.ttf'
+  },
+}
 
   // Recieve
 const handleSubmit = (event) => { 
@@ -166,6 +174,10 @@ const handleSubmit = (event) => {
           bold: true
         },
       },
+
+      defaultStyle: {
+        font: 'Roboto'
+      }
     }).print();
   })
 }
